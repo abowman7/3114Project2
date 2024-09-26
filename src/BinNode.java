e45dc83f435459fe8d38239ff47c21ce69c113ca
Adam Bowman <abowman7@vt.edu>
2024-09-26 17:56:05 -0400

    initial push

---
+++ src/BinNode.java
@@ -0,0 +1,12 @@
+interface BinNode { // Binary tree node ADT
+  // Get and set the element value
+  public Object value();
+  public void setValue(Object v);
+
+  // return the children
+  public BinNode left();
+  public BinNode right();
+
+  // return TRUE if a leaf node, FALSE otherwise
+  public boolean isLeaf();
+}
